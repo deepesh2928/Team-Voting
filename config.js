76286bde5c87628aa3d60cc976614cecf120e512
Deepesh <69512636+deepesh2928@users.noreply.github.com>
2022-05-12 12:10:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
 
-};
-// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyAEivS57KNWDn139UYZuWNjqrCN8WRnA8U",
+    authDomain: "team-voting-app-d5c7a.firebaseapp.com",
+    projectId: "team-voting-app-d5c7a",
+    storageBucket: "team-voting-app-d5c7a.appspot.com",
+    messagingSenderId: "186561731160",
+    appId: "1:186561731160:web:eb2769c5e57f7fc27b882d"
+  };
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
